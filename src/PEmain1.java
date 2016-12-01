f4d33c88f66b5a44491ee7f92b6c09aa2d67aead
MichaelStoddart <michael@apposing.co.uk>
2016-12-01 10:05:05 +0000

    Initial commit with 2 main classes

---
+++ src/PEmain1.java
@@ -0,0 +1,15 @@
+/**
+ * Created by MichaelApposing on 01/12/2016.
+ */
+public class PEmain1 {
+
+    public static void main(String[] args) {
+
+        System.out.println("MAIN 1");
+
+
+
+    }
+
+
+}
---
+++ src/PEmain2.java
@@ -0,0 +1,9 @@
+/**
+ * Created by MichaelApposing on 01/12/2016.
+ */
+public class PEmain2 {
+
+    public static void main(String[] args) {
+        System.out.println("MAIN 2");
+    }
+}
