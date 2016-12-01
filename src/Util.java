8f7315fd4fcb075bbfbe0b8a18fa6cd76bbaac94
MichaelStoddart <michael@apposing.co.uk>
2016-12-01 10:32:34 +0000

    Solutions to first 2 project Euler problems

---
+++ src/Util.java
@@ -0,0 +1,10 @@
+/**
+ * Created by MichaelApposing on 01/12/2016.
+ */
+public class Util {
+
+    public static void printOut(String TAG, Object object) {
+        System.out.println(TAG + " >>> " + object.toString());
+    }
+
+}
